0d1195fd9fa722def14e962dd40b9fdb4626c323
fumikito <guy@hametuha.com>
2024-05-17 22:30:58 +0900

    Add description.

--- includes/TaroSitemapAdmin.php
+++ includes/TaroSitemapAdmin.php
@@ -56,15 +56,24 @@ class TaroSitemapAdmin extends TaroSitemapSingleton {
 		}
 		// Section.
 		add_settings_section( 'ts_esm_section', __( 'Settings', 'ts-esm' ), function() {
-
 		}, $this->slug );
 		// Fields.
 		add_settings_field( static::OPTION_KEY, __( 'URLs', 'ts-esm' ), function() {
+			$placeholders = implode( "\n", [
+				'e.g.',
+				'https://example.com/article/123/?fbcid=1234567890',
+				'https://xample.com/article/1234/plugin-not-used/',
+			] );
 			printf(
-				'<textarea name="%s" rows="20" style="%s">%s</textarea>',
+				'<textarea name="%s" rows="20" style="%s" placeholder="%s">%s</textarea>',
 				esc_attr( static::OPTION_KEY ),
 				esc_attr( 'width: 100%; box-sizing: border-box;' ),
+				esc_attr( $placeholders ),
 				esc_textarea( $this->get_option_value() )
+			);
+			printf(
+				'<p class="description">%s</p>',
+				esc_html__( 'Enter URLs to include in the exclusive sitemap.', 'ts-esm' )
 			);
 		}, $this->slug, 'ts_esm_section' );
 		register_setting( $this->slug, static::OPTION_KEY );
@@ -74,6 +83,11 @@ class TaroSitemapAdmin extends TaroSitemapSingleton {
 				'<input type="url" value="%s" readonly style="%s" />',
 				esc_url( $this->sitemap_url() ),
 				esc_attr( 'width: 100%; box-sizing: border-box;' ),
+			);
+			printf(
+				'<p class="description">%s &raquo; %s</p>',
+				esc_html__( 'Register URL above on Google Search Console.', 'ts-esm' ),
+				sprintf( '<a href="%s" target="_blank" rel="noopener noreferrer">%s</a>', $this->sitemap_url(), __( 'Preview', 'ts-esm' ) )
 			);
 		}, $this->slug, 'ts_esm_section' );
 
